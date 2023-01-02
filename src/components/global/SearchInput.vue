53f2a159b396973cdba27cffd6fe52d12a236091
Kris Tung <ragdoll.washoku89@gmail.com>
2023-01-03 01:24:21 +0800

    建立 & 加入 SearchInput 元件樣式

--- src/components/global/SearchInput.vue
+++ src/components/global/SearchInput.vue
@@ -1,9 +1,21 @@
 <template>
   <div>
     <h3>Search Input Demo</h3>
-    <input type="text" placeholder="搜尋關鍵字" /><SvgIcon name="search"></SvgIcon>
+    <div class="search_group">
+      <input type="text" placeholder="搜尋關鍵字" class="search_input" />
+      <div class="search_execute">
+        <SvgIcon name="search"></SvgIcon>
+      </div>
+      <div class="search_close">
+        <SvgIcon class="close" name="close"></SvgIcon>
+      </div>
+    </div>
+
     <br />
-    <input type="text" placeholder="搜尋關鍵字" /><SvgIcon class="close" name="close"></SvgIcon>
+    <!-- <input type="text" placeholder="搜尋關鍵字" />
+    <div class="search_close">
+      <SvgIcon class="close" name="close"></SvgIcon>
+    </div> -->
   </div>
 </template>
 <script>
@@ -14,6 +26,40 @@ export default {
 <script setup></script>
 <style lang="scss" scoped>
 .close {
-  color: black;
+  color: $secondary-3;
+}
+
+.search {
+  &_group {
+    display: flex;
+    justify-content: space-between;
+    padding: 8px 12px;
+    width: 306px;
+    background-color: $gray;
+    border: 1px solid $line;
+    border-radius: 8px;
+  }
+
+  &_input {
+    background-color: transparent;
+    border: none;
+    outline: none;
+
+    &::placeholder {
+      font-size: $content-dk-body;
+      font-family: $font;
+      color: $secondary-3;
+    }
+  }
+
+  &_execute,
+  &_close {
+    width: 25px;
+    height: 25px;
+  }
+
+  &_close {
+    display: none;
+  }
 }
 </style>
