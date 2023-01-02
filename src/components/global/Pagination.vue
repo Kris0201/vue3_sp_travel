b116bdb135fab5c91479d9a5e842108e563f2d19
Kris Tung <ragdoll.washoku89@gmail.com>
2023-01-02 20:05:28 +0800

    建立pagination元件

---
+++ src/components/global/Pagination.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h3>Pagination Demo</h3>
+    <div class="pagination">
+      <button type="button" class="solidBtn">1</button>
+      <button type="button" class="hollowBtn">2</button>
+      <button type="button" class="hollowBtn">3</button>
+      <span>...</span>
+      <button type="button" class="hollowBtn">4</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'PaginationIndex',
+}
+</script>
+<script setup></script>
+<style lang="scss" scoped>
+.pagination {
+  display: flex;
+  gap: 12px;
+}
+
+button {
+  padding: 10px 17.5px;
+  width: 44px;
+  height: 44px;
+  text-align: center;
+}
+</style>
